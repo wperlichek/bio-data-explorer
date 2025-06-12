7299ff4293cbc36b240842e938504b420af19072
wperlichek <61857706+wperlichek@users.noreply.github.com>
2025-06-11 20:06:27 -0700

    PR Comments

--- gene_explorer.py
+++ gene_explorer.py
@@ -1,44 +1,46 @@
-import sys
+from sys import exit
+from typing import Dict, Optional
 
-genes_data = "genes.txt"
+GENES_FILE = "genes.txt"
 
-gene_to_sequence = {}
-sequence_to_nucleotide_counts = {}
+# TODO::refactor into class
+# TODO:: should these be caps in the class?
+gene_to_sequence: Dict[str, str] = {}
+sequence_to_nucleotide_counts: Dict[str, Dict[str,int]] = {}
 
 
-def count_nucleotides_in_sequence(sequence="") -> None:
+def count_nucleotides_in_sequence(sequence: str = "") -> None:
     if sequence == "":
         print("Must provide non-empty sequence to count nucleotides")
     else:
         counts = {"A": 0, "C": 0, "T": 0, "G": 0}
         sequence_to_nucleotide_counts[sequence] = counts
-        for idx in range(len(sequence)):
-            nucleotide = sequence[idx]
-            if (
-                nucleotide != "A"
-                and nucleotide != "C"
-                and nucleotide != "T"
-                and nucleotide != "G"
-            ):
+        for nucleotide in sequence:
+            if nucleotide in counts:
+                sequence_to_nucleotide_counts[sequence][nucleotide] += 1
+            else:
                 print(
                     "Warning: Unknown nucleotide present in sequence "
                     + sequence
-                    + " at position "
-                    + str(idx)
-                    + " : "
+                    + ": "
                     + nucleotide
                 )
                 pass
-            else:
-                sequence_to_nucleotide_counts[sequence][nucleotide] += 1
+                
 
 
 def parse_genes_data() -> None:
     try:
-        with open(genes_data) as File:
+        with open(GENES_FILE) as File:
             lines = File.readlines()
             for line in lines:
                 gene_and_sequence = line.strip().split(":")
+
+                if (len(gene_and_sequence) != 2):
+                    print("Line " + line + " in " + GENES_FILE + 
+                          " is incorrectly formatted, will not parse it")
+                    pass
+
                 gene_name = gene_and_sequence[0]
                 gene_sequence = gene_and_sequence[1]
                 if gene_name not in gene_to_sequence:
@@ -54,19 +56,21 @@ def parse_genes_data() -> None:
                         + gene_sequence
                     )
     except FileNotFoundError as e:
-        print("Could not open " + genes_data + ": " + e.strerror)
+        # TODO :: use python logging module
+        print("Could not open " + GENES_FILE + ": " + e.strerror)
         sys.exit()
 
 
 def print_all_genes() -> None:
-    print("There are " + str(len(gene_to_sequence)) + " genes loaded: ")
+    # TODO - consistent use of f-strings
+    print(f"There are " + {len(gene_to_sequence)} + " genes loaded: ")
     number = 1
     for k, v in gene_to_sequence.items():
         print(str(number) + ": " + k)
         number += 1
 
 
-def get_gene_sequence(gene_name="") -> str:
+def get_gene_sequence(gene_name: str = "") -> Optional[str]:
     if gene_name == "":
         print("Must provide gene name to view sequence")
         return None
@@ -77,7 +81,7 @@ def get_gene_sequence(gene_name="") -> str:
         return gene_name + " sequence: " + gene_to_sequence[gene_name]
 
 
-def get_count_nucleotides(gene_name="") -> str:
+def get_count_nucleotides(gene_name: str = "") -> Optional[Dict[str, int]]:
     if gene_name == "":
         print("Must provide gene name to count nucleotides")
         return None
@@ -92,13 +96,13 @@ def get_count_nucleotides(gene_name="") -> str:
             return sequence_to_nucleotide_counts[sequence]
 
 
-def pretty_print_count_nucleotides(gene_name="", nucleotide_counts={}) -> None:
+def pretty_print_count_nucleotides(gene_name: str = "", nucleotide_counts: Optional[Dict[str, int]] = None) -> None:
     if not nucleotide_counts:
         print("Must provide nucleotide count map to print")
     else:
         pretty_printed = ""
         for k, v in nucleotide_counts.items():
-            pretty_printed = pretty_printed + k + "=" + str(v) + " "
+            pretty_printed += f"{k}={v} "
         print(gene_name + ": " + pretty_printed)
 
 
@@ -114,22 +118,24 @@ def cli_app() -> None:
         print("4. Exit application")
 
         user_input = input("Enter choice (1-4): ").strip()
-        res = ""
+        result = ""
 
         if user_input == "1":
             print_all_genes()
         elif user_input == "2":
-            user_input = input("Enter gene name: ")
-            res = get_gene_sequence(user_input).strip()
-            print(res)
+            user_input = input("Enter gene name: ").strip()
+            result = get_gene_sequence(user_input)
+            if result:
+                print(result)
         elif user_input == "3":
             user_input = input("Enter gene name: ").strip()
-            res = get_count_nucleotides(user_input)
-            pretty_print_count_nucleotides(user_input, res)
+            result = get_count_nucleotides(user_input)
+            if result:
+                pretty_print_count_nucleotides(user_input, result)
         elif user_input == "4":
-            sys.exit()
+            break
         else:
             print("Invalid choice")
 
-
+# TODO :: wrap in standard python startup idiom
 cli_app()
