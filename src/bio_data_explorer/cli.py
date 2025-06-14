cebcc5e305ffd7ebc033682bf99b63d4a81b5a82
Will <williamperlichek@gmail.con>
2025-06-14 07:26:18 -0700

    modularize step 1

---
+++ src/bio_data_explorer/cli.py
@@ -0,0 +1,55 @@
+import sys
+import logging
+from gene_explorer import GenesExplorer
+from fasta_parser import parse_genes_data, GenesFileParsingError
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+GENES_FILE = "sample_genes.fasta.gz"
+
+def main() -> None: 
+    logging.info("Starting app")
+
+    try:
+        genes = parse_genes_data(GENES_FILE)
+    except GenesFileParsingError as e:
+        logging.critical(f"Application can't start due to {e}, exiting application")
+        sys.exit(1)
+
+    genes_explorer = GenesExplorer(genes)
+
+    while True:
+        print("** Gene Sequence Explorer **")
+        print("1. List all genes")
+        print("2. View sequence of gene")
+        print("3. Count nucleotides of gene")
+        print("4. Exit application")
+
+        menu_choice = input("Enter choice (1-4): ").strip()
+
+        if menu_choice == "1":
+            genes_explorer.print_all_genes()
+        elif menu_choice == "2":
+            gene_name = input("Enter gene name: ").strip().lower()
+            result = genes_explorer.get_gene_sequence(gene_name)
+            if result:
+                print(
+                    f"{genes_explorer.get_gene_name_original_casing(gene_name)}: {result}"
+                )
+        elif menu_choice == "3":
+            gene_name = input("Enter gene name: ").strip().lower()
+            sequence = genes_explorer.get_gene_sequence(gene_name)
+            if sequence:
+                result = genes_explorer.get_count_nucleotides(sequence)
+                if result:
+                    genes_explorer.pretty_print_count_nucleotides(gene_name, result)
+        elif menu_choice == "4":
+            logging.info("Exiting app")
+            break
+        else:
+            print("Invalid choice")
+
+if __name__ == "__main__":
+    main()
--- src/bio_data_explorer/gene_explorer.py
+++ src/bio_data_explorer/gene_explorer.py
@@ -1,21 +1,5 @@
-import logging, sys, gzip
+import logging
 from typing import Dict, Optional, List
-from pathlib import Path
-
-data_directory_path = Path(__file__).resolve().parent.parent.parent / "data"
-
-GENES_FILE = "sample_genes.fasta.gz"
-
-FASTA_SEQUENCE_CHARS_DNA = set("ACGT")
-
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
-)
-
-
-class GenesFileParsingError(Exception):
-    pass
-
 
 class Gene:
     def __init__(self, identifier: str = "", description: str = "", sequence: str = ""):
@@ -23,9 +7,7 @@ class Gene:
         self.description = description
         self.sequence = sequence
 
-
 class GenesExplorer:
-
     def __init__(self, genes: Optional[List[Gene]] = None):
         self.gene_to_sequence: Dict[str, str] = {}
         self.sequence_to_nucleotide_counts: Dict[str, Dict[str, int]] = {}
@@ -98,7 +80,6 @@ class GenesExplorer:
             return self.sequence_to_nucleotide_counts[sequence.upper()]
 
     def print_all_genes(self) -> None:
-        # TODO :: print the description too
         print(f"There are {len(self.gene_name_casing_map)} genes loaded: ")
         number = 1
         for k, v in self.gene_name_casing_map.items():
@@ -116,116 +97,3 @@ class GenesExplorer:
             print(f"{self.get_gene_name_original_casing(gene_name)}: {pretty_printed}")
 
 
-def parse_genes_data(genes_file: str = "") -> List[Gene]:
-    genes: List[Gene] = []
-    try:
-        with gzip.open(
-            f"{data_directory_path}/{genes_file}",
-            "rt",
-            encoding="utf-8",
-        ) as File:
-            identifier = ""
-            description = ""
-            sequence = ""
-            found_format_problem = False
-            for line in File:
-                stripped_line = line.strip()
-                if not stripped_line:
-                    pass
-                else:
-                    if line_is_formatted_correctly(stripped_line):
-                        if stripped_line[0] == ">":
-                            if sequence:
-                                if not found_format_problem:
-                                    genes.append(
-                                        Gene(identifier, description, sequence.upper())
-                                    )
-                                else:
-                                    found_format_problem = False
-                            identifier_and_description = (
-                                stripped_line[1::].strip().split(" ")
-                            )
-                            identifier = identifier_and_description[0]
-                            description = " ".join(identifier_and_description[1::])
-                            sequence = ""
-                        else:
-                            sequence += stripped_line
-                    else:
-                        logging.warning(
-                            f"{line} is not formatted correctly, skipping this entire FASTA entry"
-                        )
-                        if sequence and not found_format_problem:
-                            genes.append(
-                                Gene(identifier, description, sequence.upper())
-                            )
-                        found_format_problem = True
-            if not found_format_problem:
-                genes.append(Gene(identifier, description, sequence.upper()))
-        return genes
-    except FileNotFoundError as e:
-        logging.error(f"Could not open {genes_file}: {e.strerror}")
-        raise GenesFileParsingError(e)
-
-
-def line_is_formatted_correctly(line: str = "") -> bool:
-    if line[0] == ">":
-        there_are_contents = len(line) > 1
-        if there_are_contents:
-            return True
-        else:
-            logging.warning(f"Line {line} is missing identifier")
-            return False
-    else:
-        for ch in line:
-            if ch not in FASTA_SEQUENCE_CHARS_DNA:
-                logging.warning(f"Found non-FASTA character {ch} in {line}")
-                return False
-        return True
-
-
-def cli_app() -> None:
-
-    logging.info("Starting app")
-
-    try:
-        genes = parse_genes_data(GENES_FILE)
-    except GenesFileParsingError as e:
-        logging.critical(f"Application can't start due to {e}, exiting application")
-        sys.exit(1)
-
-    genes_explorer = GenesExplorer(genes)
-
-    while True:
-        print("** Gene Sequence Explorer **")
-        print("1. List all genes")
-        print("2. View sequence of gene")
-        print("3. Count nucleotides of gene")
-        print("4. Exit application")
-
-        menu_choice = input("Enter choice (1-4): ").strip()
-
-        if menu_choice == "1":
-            genes_explorer.print_all_genes()
-        elif menu_choice == "2":
-            gene_name = input("Enter gene name: ").strip().lower()
-            result = genes_explorer.get_gene_sequence(gene_name)
-            if result:
-                print(
-                    f"{genes_explorer.get_gene_name_original_casing(gene_name)}: {result}"
-                )
-        elif menu_choice == "3":
-            gene_name = input("Enter gene name: ").strip().lower()
-            sequence = genes_explorer.get_gene_sequence(gene_name)
-            if sequence:
-                result = genes_explorer.get_count_nucleotides(sequence)
-                if result:
-                    genes_explorer.pretty_print_count_nucleotides(gene_name, result)
-        elif menu_choice == "4":
-            logging.info("Exiting app")
-            break
-        else:
-            print("Invalid choice")
-
-
-if __name__ == "__main__":
-    cli_app()
