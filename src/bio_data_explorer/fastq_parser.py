e742562e3b21c58b47e8768b46efe0c3c3a00a8a
Will <williamperlichek@gmail.com>
2025-06-16 10:16:39 -0700

    work on fastq parsing

--- src/bio_data_explorer/fastq_parser.py
+++ src/bio_data_explorer/fastq_parser.py
@@ -17,14 +17,14 @@ class FastqParsingError(Exception):
 
 def parse_fastq_file(fastq_file_name: str = "") -> List[Gene]:
     try:
-        with gzip.open(fastq_file_name) as fastq_file:
+        with gzip.open(f"{data_directory_path}/{fastq_file_name}") as fastq_file:
+            genes: List[Gene] = []
             try:
                 # https://biopython.org/wiki/SeqIO
                 for record in SeqIO.parse(fastq_file, FILE_TYPE_FASTQ):  # type: ignore
                     logger.info(f"Parsed record: {record.name}")  # type: ignore
             except Exception as e:
                 logger.error(f"Could not parse fastq file: {e}")
-            genes: List[Gene] = []
             return genes
     except FileNotFoundError as e:
         logger.error(f"Could not open {fastq_file_name}: {e.strerror}")
