fa3144ed8fd0b768a4e1c21e87d7d57763f8f558
Will <williamperlichek@gmail.com>
2025-06-17 16:43:32 -0700

    basic filtering code

--- src/bio_data_explorer/vcf_parser.py
+++ src/bio_data_explorer/vcf_parser.py
@@ -1,17 +1,50 @@
 import logging
 from pathlib import Path
-import cyvcf2  # type: ignore
 from cyvcf2 import VCF  # type: ignore
 
 data_directory_path = Path(__file__).resolve().parent.parent.parent / "data"
 
 logger = logging.getLogger(__name__)
 
+MIN_QUAL_SCORE = 30.0
+MIN_INFO_DP = 20
+
 
 def parse_vcf_file(vcf_file_name: str = ""):
-    print("cyvcf2 version:", cyvcf2.__version__)
     try:
-        for variant in VCF(f"{data_directory_path}/{vcf_file_name}"):  # type: ignore
-            print(variant.REF)  # type: ignore
+        vcf = VCF(f"{data_directory_path}/{vcf_file_name}")  # type: ignore
     except Exception as e:
         logger.error(f"Could not open {vcf_file_name}: {e}")
+
+    for variant in vcf:  # type: ignore
+        # https://brentp.github.io/cyvcf2/
+        # https://brentp.github.io/cyvcf2/docstrings.html#api
+        filter = str(variant.FILTER)  # type: ignore
+        qual = float(variant.QUAL)  # type: ignore
+        info_dp = int(variant.INFO.get("DP"))  # type: ignore
+        variant_identifer = f"{variant.CHROM}:{variant.POS}_{variant.REF}>{" ".join(variant.ALT)}"  # type: ignore
+        if high_confidence_variant(filter, qual, info_dp, variant_identifer):  # type: ignore
+            print("high confidence variant")
+        else:
+            logger.warning(f"Filtering out variant {variant_identifer}")
+
+
+def high_confidence_variant(
+    filter: str = "", qual: float = 0.0, info_dp: int = 0, variant_identifer: str = ""
+) -> bool:
+    if filter != "None":
+        logger.warning(
+            f"Variant {variant_identifer} did not PASS in filter, filter value: {filter}"
+        )
+        return False
+    if qual < MIN_QUAL_SCORE:
+        logger.warning(
+            f"Variant {variant_identifer} qual score {qual} does not meet min qual threshold {MIN_QUAL_SCORE}"
+        )
+        return False
+    if info_dp < MIN_INFO_DP:
+        logger.warning(
+            f"Variant {variant_identifer} depth (info_dp) {info_dp} does not meet min depth threshold {MIN_INFO_DP}"
+        )
+        return False
+    return True
