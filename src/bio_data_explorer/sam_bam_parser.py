cd0151d6cfe7e0adffed3fa113030180a004749e
Will <williamperlichek@gmail.com>
2025-06-18 10:18:16 -0700

    bai indexing

--- src/bio_data_explorer/sam_bam_parser.py
+++ src/bio_data_explorer/sam_bam_parser.py
@@ -11,16 +11,28 @@ class SamBamParsingError(Exception):
     pass
 
 
-def parse_sam_or_bam_file(sam_file_name: str = ""):
-    mode = "r" if Path(sam_file_name).suffix == ".sam" else "rb"
+def parse_sam_or_bam_file(sam_or_sam_file_name: str = ""):
+    mode = "r" if Path(sam_or_sam_file_name).suffix == ".sam" else "rb"
     try:
-        sam_file = pysam.AlignmentFile(f"{data_directory_path}/{sam_file_name}", mode)
+        sam_file = pysam.AlignmentFile(
+            f"{data_directory_path}/{sam_or_sam_file_name}", mode
+        )
     except Exception as e:
-        logger.error(f"Could not open {sam_file_name}: {e}")
+        logger.error(f"Could not open {sam_or_sam_file_name}: {e}")
         raise SamBamParsingError(e)
 
     for read in sam_file:
         print(read)
 
     sam_file.close()
-    return
+
+
+def create_bai_from_bam_file(bam_file_name: str = ""):
+    bai_index_file = f"{data_directory_path}/sorted_{bam_file_name}"
+    logger.info(f"Creating index for {bam_file_name} at {bai_index_file}")
+    pysam.sort(
+        "-o",
+        bai_index_file,
+        f"{data_directory_path}/{bam_file_name}",
+    )
+    pysam.index(bai_index_file)
