f264888db71e225f341acbe630bf3d28bc6eff3a
Will <williamperlichek@gmail.com>
2025-06-17 17:13:47 -0700

    CLI title

--- src/bio_data_explorer/cli.py
+++ src/bio_data_explorer/cli.py
@@ -29,7 +29,7 @@ def main() -> None:
     genes_explorer = GenesExplorer(genes)
 
     while True:
-        print("** Gene Sequence Explorer **")
+        print("** Bio Data Explorer **")
         print("1. List all genes")
         print("2. View sequence of gene")
         print("3. Count nucleotides of gene")
