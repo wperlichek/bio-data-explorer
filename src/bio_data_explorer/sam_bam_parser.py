90b282de0d54b2d5ab5ba2151d745eaa90000893
Will <williamperlichek@gmail.com>
2025-06-18 15:04:15 -0700

    more stats analysis of reads

--- src/bio_data_explorer/sam_bam_parser.py
+++ src/bio_data_explorer/sam_bam_parser.py
@@ -8,6 +8,8 @@ data_directory_path = Path(__file__).resolve().parent.parent.parent / "data"
 
 logger = logging.getLogger(__name__)
 
+MIN_MAPPING_QUALITY_THRESHOLD = 30
+
 
 class SamBamParsingError(Exception):
     pass
@@ -49,12 +51,26 @@ def get_read_alignment_stats_summary(alignment_file: AlignmentFile):
     reads_count = 0
     mapped_reads = 0
     unmapped_reads = 0
+    low_quality_reads = 0
+    duplicate_reads = 0
+    supplimentary_reads = 0
 
     for read in alignment_file:
         if read.is_mapped:
             mapped_reads += 1
         else:
             unmapped_reads += 1
+
+        if read.mapping_quality < MIN_MAPPING_QUALITY_THRESHOLD:
+            low_quality_reads += 1
+            logger.warning(
+                f"Read {read.query_name} map quality of {read.mapping_quality} is less than threshold of {MIN_MAPPING_QUALITY_THRESHOLD}"
+            )
+
+        if read.is_duplicate:
+            duplicate_reads += 1
+        if read.is_supplementary:
+            supplimentary_reads += 1
 
         reads_count += 1
 
@@ -66,5 +82,8 @@ def get_read_alignment_stats_summary(alignment_file: AlignmentFile):
     alignment_stats["mapped_reads"] = mapped_reads
     alignment_stats["unmapped_reads"] = unmapped_reads
     alignment_stats["mapping_rate_percentage"] = mapping_rate_percentage
+    alignment_stats["low_quality_reads"] = low_quality_reads
+    alignment_stats["duplicate_reads"] = duplicate_reads
+    alignment_stats["supplimentary_reads"] = supplimentary_reads
 
     return alignment_stats
