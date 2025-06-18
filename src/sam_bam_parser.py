b51435654da45903bf759ad3d8d1e0abaa82c0a3
Will <williamperlichek@gmail.com>
2025-06-18 08:10:37 -0700

    add samtools example

---
+++ src/sam_bam_parser.py
@@ -0,0 +1,14 @@
+import logging
+from pathlib import Path
+
+data_directory_path = Path(__file__).resolve().parent.parent.parent / "data"
+
+logger = logging.getLogger(__name__)
+
+
+def parse_sam_file():
+    return
+
+
+def parse_bam_file():
+    return
