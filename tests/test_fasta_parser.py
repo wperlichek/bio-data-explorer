9d61be3fa75ffb1c2df65842e9875e30ce3aac2d
Will <williamperlichek@gmail.com>
2025-06-19 08:36:45 -0700

    keep working on tests

--- tests/test_fasta_parser.py
+++ tests/test_fasta_parser.py
@@ -19,10 +19,10 @@ def test_parse_fasta_file_exludes_records_missing_identifier():
 
 
 def test_parse_fasta_file_exludes_records_with_non_dna_bases():
-    file_name = "sample_genes_bad_bases.fasta.gz"
+    file_name = "sample_genes_bad_sequences.fasta.gz"
     original_record_count = 11
     expected_genes_parsed = (
-        original_record_count - 3
-    )  # contains 3 records with bases that are non-DNA bases
+        original_record_count - 1  # contains 1 record with base that is non-DNA base
+    )
     genes = fp.parse_fasta_file(f"{test_path_config.DATA_DIR}/{file_name}")
     assert len(genes) == expected_genes_parsed
