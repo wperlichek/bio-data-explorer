106fda0958698315e2d7cd0196127002bee0bb3c
Will <williamperlichek@gmail.con>
2025-06-19 06:35:41 -0700

    test adjust

--- tests/test_fasta_parser.py
+++ tests/test_fasta_parser.py
@@ -2,6 +2,6 @@ import pytest
 from bio_data_explorer import fasta_parser as fp
 
 
-def test_line_is_formatted_correctly_missing_identifier():
-    with pytest.raises(fp.FastaParsingError) as exc_info:
+def test_parse_fasta_file_missing_file():
+    with pytest.raises(fp.FastaParsingError):
         fp.parse_fasta_file("file_doesnt_exist.txt")
