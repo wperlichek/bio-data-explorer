178463a918b9b15a1885b094f6ecc8cb60f1a27e
Will <williamperlichek@gmail.con>
2025-06-19 16:20:30 -0700

    iterator test

--- tests/test_blast_client.py
+++ tests/test_blast_client.py
@@ -2,6 +2,8 @@ from _pytest.logging import LogCaptureFixture
 from unittest.mock import patch
 from bio_data_explorer import blast_client as bc
 import logging
+from typing import Iterator
+from Bio.Blast import Record
 
 
 def test_make_blast_call_empty_sequence(caplog: LogCaptureFixture):
@@ -11,11 +13,19 @@ def test_make_blast_call_empty_sequence(caplog: LogCaptureFixture):
     assert "Must provide sequence to make a BLAST call" in caplog.text
 
 
-def test_make_blast_uses_qblast():
-    with patch("bio_data_explorer.blast_client.NCBIWWW.qblast") as mock_call:
+def test_make_blast_gets_blast_search_results():
+    with (
+        patch("bio_data_explorer.blast_client.NCBIWWW.qblast") as mock_qblast_call,
+        patch("bio_data_explorer.blast_client.NCBIXML.parse") as mock_parse_call,
+    ):
         seq = "AGCTAG"
-        mock_call.return_value = "mock_handle"
+        mock_handle = "mock_handle"
+        stub_record_iterator: Iterator[Record] = iter([Record()])
+        mock_qblast_call.return_value = mock_handle
+        mock_parse_call.return_value = stub_record_iterator
         result = bc.make_blast_call(bc.BlastProgram.BLASTN, bc.BlastDatabase.NT, seq)
-        mock_call.assert_called_once_with(
-            bc.BlastProgram.BLASTN, bc.BlastDatabase.NT, seq
+        mock_qblast_call.assert_called_once_with(
+            bc.BlastProgram.BLASTN.value, bc.BlastDatabase.NT.value, seq
         )
+        mock_parse_call.assert_called_once_with(mock_handle)
+        assert result is stub_record_iterator
