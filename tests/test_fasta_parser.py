ec00b349367a76716d9949bce3d418047c96d847
Will <williamperlichek@gmail.con>
2025-06-19 06:55:48 -0700

    test stub

--- tests/test_fasta_parser.py
+++ tests/test_fasta_parser.py
@@ -5,3 +5,7 @@ from bio_data_explorer import fasta_parser as fp
 def test_parse_fasta_file_missing_file():
     with pytest.raises(fp.FastaParsingError):
         fp.parse_fasta_file("file_doesnt_exist.txt")
+
+
+def test_parse_fasta_file_exludes_records_missing_identifier():
+    return
