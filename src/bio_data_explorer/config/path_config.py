7943a7ee4c08ea813f1f508846083369f4642356
Will <williamperlichek@gmail.com>
2025-06-19 10:56:21 -0700

    dir structure

--- src/bio_data_explorer/config/path_config.py
+++ src/bio_data_explorer/config/path_config.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).resolve().parents[3]
-
 DATA_DIR = PROJECT_ROOT / "data"
+FASTA_PATH = DATA_DIR / "fasta"
+FASTQ_PATH = DATA_DIR / "fastq"
+SAM_BAM_PATH = DATA_DIR / "sam-bam"
+VCF_PATH = DATA_DIR / "vcf"
