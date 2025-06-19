6f3e5a756774058be6265e43f762ab8d21dd9698
Will <williamperlichek@gmail.com>
2025-06-19 10:42:04 -0700

    len too short test

--- tests/test_fastq_parser.py
+++ tests/test_fastq_parser.py
@@ -2,33 +2,15 @@ from bio_data_explorer import fastq_parser as fp
 from .config import path_config as test_path_config
 
 
-def test_test_parse_fastq_file_keeps_good_reads():
+def test_parse_fastq_file_keeps_good_reads():
     file_name = "sample_genes_good_format.fastq.gz"
     expected_genes_parsed = 5
     genes = fp.parse_fastq_file(f"{test_path_config.FASTQ_PATH}/{file_name}")
     assert len(genes) == expected_genes_parsed
 
 
-# def test_quality_trim_sequence_end_retains_original_for_good_reads():
-#     file_name = "sample_genes_good_format.fasta.gz"
-#     expected_genes_parsed = 11
-#     genes = fp.parse_fasta_file(f"{test_path_config.DATA_DIR}/{file_name}")
-#     assert actual_sequence == fqp._quality_trim_sequence_end(
-#         actual_sequence, [20, 20, 20, 40, 20]
-#     )
-
-
-# def test_quality_trim_sequence_end_trims_trailing():
-#     actual_sequence = "TCTGA"
-#     expected_sequence = "TC"
-#     assert expected_sequence == fqp._quality_trim_sequence_end(
-#         actual_sequence, [20, 20, 10, 40, 20]
-#     )
-
-
-# def test_quality_trim_sequence_end_trims_from_first_bad_read():
-#     actual_sequence = "TCTGA"
-#     expected_sequence = "TC"
-#     assert expected_sequence == fqp._quality_trim_sequence_end(
-#         actual_sequence, [20, 20, 10, 40, 20]
-#     )
+def test_parse_fastq_file_discards_reads_with_len_too_short():
+    file_name = "sample_genes_short_length.fastq.gz"
+    expected_genes_parsed = 1
+    genes = fp.parse_fastq_file(f"{test_path_config.FASTQ_PATH}/{file_name}")
+    assert len(genes) == expected_genes_parsed
