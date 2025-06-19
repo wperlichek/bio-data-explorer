06bddb98610f3a46e437871ebfc66077329cc6a2
Will <williamperlichek@gmail.con>
2025-06-19 15:38:07 -0700

    blast client

--- src/bio_data_explorer/blast_client.py
+++ src/bio_data_explorer/blast_client.py
@@ -1,6 +1,6 @@
-from Bio.Blast import NCBIWWW, NCBIXML
+from Bio.Blast import NCBIWWW, NCBIXML, Record
 from enum import Enum
-from typing import List, Optional, Any
+from typing import Iterator, Optional
 import logging
 
 logger = logging.getLogger(__name__)
@@ -18,7 +18,7 @@ def make_blast_call(
     program: Optional[BlastProgram] = None,
     database: Optional[BlastDatabase] = None,
     sequence: str = "",
-) -> Optional[List[Any]]:
+) -> Optional[Iterator[Record]]:
     if sequence == "":
         logger.warning("Must provide sequence to make a BLAST call")
         return None
@@ -27,16 +27,16 @@ def make_blast_call(
     if database is None:
         database = BlastDatabase.NT
     try:
-        result_handle = NCBIWWW.qblast(program.value, database.value, sequence)  # type: ignore[reportUnknownMemberType]
+        result_handle = NCBIWWW.qblast(program.value, database.value, sequence)
     except Exception as e:
         logger.error(f"Problem during external call to {NCBIWWW.NCBI_BLAST_URL}: {e}")
         return None
 
     try:
-        # https://biopython.org/docs/1.76/api/Bio.Blast.Record.html
-        blast_records = NCBIXML.parse(result_handle)  # type: ignore
+        # See https://biopython.org/docs/1.76/api/Bio.Blast.Record.html
+        blast_record = NCBIXML.parse(result_handle)
     except Exception as e:
         logger.error(f"Problem parsing result handle {result_handle.getvalue}: {e}")
         return None
 
-    return blast_records  # type: ignore
+    return blast_record
